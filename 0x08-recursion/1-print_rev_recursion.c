9f3d3b5fffc45d1741bb6726363f9d2fef3f44c7
Xavviieerr <ogbupaul545@gmail.com>
2024-01-10 11:23:35 -0800

    printing in reverse function

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_print_rev_recursion - prints in reverse
+ *@s: string
+ *Return: returns
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{	
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
