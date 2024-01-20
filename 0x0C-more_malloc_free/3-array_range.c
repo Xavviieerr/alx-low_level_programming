f6c60aac229daf9813414dcfd28e41c577f140c5
Xavviieerr <ogbupaul545@gmail.com>
2024-01-20 05:48:48 -0800

    fixed memory error

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,36 +1,29 @@
 #include "main.h"
+
 /**
- *array_range - range of values
- *@min: minimum
- *@max: maximum
- *Return: returns pointer
+ * *array_range - creates an array of integers
+ * @min: minimum range of values stored
+ * @max: maximum range of values stored and number of elements
+ *
+ * Return: pointer to the new array
  */
 int *array_range(int min, int max)
 {
-	int *arr;
-	int count, i, copy;
+	int *ptr;
+	int i, size;
 
-	count = 0, i = 0;
-	copy = min;
 	if (min > max)
-	{
-		return (NULL);
-	}
-	if (copy != max)
-	{
-		copy++;
-		count++;
-	}
-	arr = malloc(sizeof(int) * count + 2);
-	if (arr == NULL)
-	{
-		return (NULL);
-	}
-	while (min <= max)
-	{
-		arr[i] = min;
-		min++;
-		i++;
-	}
-	return (arr);
+		return (0);
+
+	size = max - min + 1;
+
+	ptr = malloc(sizeof(int) * size);
+
+	if (ptr == NULL)
+		return (0);
+
+	for (i = 0; min <= max; i++)
+		ptr[i] = min++;
+
+	return (ptr);
 }
