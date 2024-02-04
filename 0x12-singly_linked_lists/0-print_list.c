c98a75c36bce1c49ef50b6838f8343ec17f31199
Xavviieerr <ogbupaul545@gmail.com>
2024-02-04 11:16:22 -0800

    task 1

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *print_list - prints the contents of a linked list
+ *@h: start of the linked list
+ @Return: returns
+ */
+size_t print_list(const list_t *h)
+{
+	int count;
+	const list_t *currentnode = h;
+
+	count = 0;
+	while (currentnode != NULL)
+	{
+		if (currentnode->str == NULL)
+		{
+			printf("[%d] (nil)\n", 0);
+		}
+		else
+		{
+			printf("[%d] %s\n", currentnode->len, currentnode->str);
+		}
+		count++;
+		currentnode = currentnode->next;
+	}
+	return (count);
+}
